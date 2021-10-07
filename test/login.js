4c50254e3d18a3ed14bf44a82a50daad3ac38930
FilipeConde <filipecondep@gmail.com>
2021-10-06 22:35:20 -0300

    create a few ugly request tests

---
+++ test/login.js
@@ -0,0 +1,47 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+chai.use(chaiHttp);
+
+describe('Algumas requisições FEIAS', () => {
+  it('Deve rodar uma requisição FEIA do tipo GET', () => {
+    // console.log('Hello bug');
+    chai
+      .request('http://localhost:3000')
+      .get('/produtos')
+      .end((err, res) => {
+        console.log(res.body);
+      });
+  });
+
+  it('Deve rodar uma requisição FEIA do tipo POST', () => {
+    // console.log('Hello bug');
+    chai
+      .request('http://localhost:3000')
+      .post('/login')
+      .send({
+        email: 'fulano@qa.com',
+        password: 'teste',
+      })
+      .end((err, res) => {
+        console.log(res.body);
+      });
+  });
+
+  it.only('Só pra dar agonia, mais uma requisição FEIA', () => {
+    // console.log('Hello bug');
+    chai
+      .request('http://localhost:3000')
+      .post('/produtos')
+      .send({
+        nome: 'Caneta azul',
+        preco: 470,
+        descricao: 'Azul caneta',
+        quantidade: 381,
+      })
+      .set('Authorization', 'Bearer token mágico que abre portas')
+      .end((err, res) => {
+        console.log(res.body);
+      });
+  });
+});
